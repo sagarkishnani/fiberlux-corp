1c7d12b13e24fd432749a53bd5494a20a0a2c126
Sagar <sagar@seekrwellness.com>
2026-07-03 00:32:14 -0500

    fix. mediaroot

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'd0a21eadf7d9b6e556c57fafcf176510bfa2657a', queries,  });
+export const client = createClient({ cacheDir: '/Users/sagarkishnanigarcia/Documents/Proyectos/Twinstudios/Fiberlux/Web/fiberlux-corp/tina/__generated__/.cache/1783055326166', url: 'https://content.tinajs.io/1.6/content/b7c4f7b2-044d-45ce-ad83-b851ea96927b/github/main', token: '002b0ead3d9a6042b0422e8ddd76afc87556f499', queries,  });
 export default client;
   
