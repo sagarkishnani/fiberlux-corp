125ff267851e7d9898f653cbe6130622dba22e00
Sagar <sagar@seekrwellness.com>
2026-07-22 09:35:53 -0500

    fix.catalogo: canalon entre paginas para que al arrastrar no se peguen los bloques (obs5)

--- src/components/servicios/CatalogoSolucionesReact.tsx
+++ src/components/servicios/CatalogoSolucionesReact.tsx
@@ -222,7 +222,7 @@ export default function CatalogoSolucionesReact({
         <div className="md:hidden">
           <div
             ref={slider.ref}
-            className="catalogo-scroll flex overflow-x-auto snap-x snap-mandatory select-none"
+            className="catalogo-scroll flex gap-6 overflow-x-auto snap-x snap-mandatory select-none"
             style={{ cursor: pageCount > 1 ? "grab" : "default" }}
             {...slider.handlers}
           >
