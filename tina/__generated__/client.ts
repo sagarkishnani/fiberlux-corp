40ccc1271bf72efa32d9bb6d8d1b8cca62f9dafd
Sagar <sagar@seekrwellness.com>
2026-08-07 01:58:49 -0500

    fix. home hero

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '002b0ead3d9a6042b0422e8ddd76afc87556f499', queries,  });
 export default client;
   
